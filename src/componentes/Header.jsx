c4b0150662a8dffc8eb76bd2775ae2bd5e2611e2
erik godoy <sebastianerik802@gmail.com>
2022-08-16 19:13:14 -0300

    aprendiendo git amigo

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <>
       <h1 className="font-black text-4xl text-center md:w-2/3 mx-auto">
-        Seguimiento de pacientes
+        Seguimiento de pacientes , con github {" "} aprendiendo perri
          <span className="text-indigo-600">Veterinaria </span>
       </h1>
     </>
